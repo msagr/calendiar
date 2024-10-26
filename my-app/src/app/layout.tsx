e41a2f33370388faa9f10fc5a8462bf9b2db2c06
msagr <mehulsagrawal33@gmail.com>
2024-10-27 02:06:28 +0530

    clerk auth setup

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -16,12 +17,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <ClerkProvider>
+      <html lang="en">
       <body
         className={cn("min-h-screen, bg-background font-sans antialiased", inter.variable)}
       >
         {children}
       </body>
     </html>
+    </ClerkProvider>
   );
 }
