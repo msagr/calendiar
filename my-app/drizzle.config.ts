12411e3444c91efc698727344a576196aea516f6
msagr <mehulsagrawal33@gmail.com>
2024-10-28 00:25:29 +0530

    added events page

--- my-app/drizzle.config.ts
+++ my-app/drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle/migrations',
-  schema: './drizzle/schema.ts',
+  out: '.src/drizzle/migrations',
+  schema: '.src/drizzle/schema.ts',
   dialect: 'postgresql',
   strict: true,
   verbose: true,
